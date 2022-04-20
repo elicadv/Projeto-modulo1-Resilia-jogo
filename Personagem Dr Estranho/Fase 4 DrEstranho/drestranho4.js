af57f6bbded8a80ee4c46d0178233e10facd0f90
Elica <elicavb@gmail.com>
2022-04-20 17:04:28 -0300

    Meu primeiro jogo

---
+++ Personagem Dr Estranho/Fase 4 DrEstranho/drestranho4.js
@@ -0,0 +1,16 @@
+
+function respostaCerta(){
+    while(opcao !=1 || opcao!=2 ){
+    var opcao = prompt("Digite a resposta certa!")
+        if(opcao == 2){
+        window.location.href = "file:///C:/Users/Resilia/OneDrive/Documentos/Projeto%20jogo%20Resilia/Personagem%20Dr%20Estranho/Fase%204%20DrEstranho/gameover.html"
+        break;
+    }else if (opcao == 1){
+        window.location.href = "file:///C:/Users/Resilia/OneDrive/Documentos/Projeto%20jogo%20Resilia/Personagem%20Dr%20Estranho/Fase%204%20DrEstranho/parabens.html"
+        break;
+    }else { 
+            alert("Digite uma opção válida, 1 ou 2!")
+    }
+    }
+    }
+    
