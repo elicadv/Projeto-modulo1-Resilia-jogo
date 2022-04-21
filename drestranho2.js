c180d4b8fcd97e3ae7d95f509ca631816aba8d1a
Elica <elicavb@gmail.com>
2022-04-21 19:36:54 -0300

    Meu primeiro jogo

---
+++ drestranho2.js
@@ -0,0 +1,16 @@
+
+function respostaCerta(){
+    while(opcao !=1 || opcao!=2 ){
+    var opcao = prompt("Digite a resposta certa!")
+        if(opcao == 2){
+        window.location.href = "drestranho3.html"
+        break;
+    }else if (opcao == 1){
+        window.location.href = "gameover.html"
+        break;
+    }else { 
+            alert("Digite uma opção válida, 1 ou 2!")
+    }
+    }
+    }
+    
---
+++ fase3hulck.js
@@ -0,0 +1,22 @@
+
+function respostaCerta(){
+    while(opcao !=1 || opcao!=2 ){
+    var opcao = prompt("Digite a resposta certa!")
+        if(opcao == 2){
+        window.location.href = "gameover.html"
+        break;
+    }else if (opcao == 1){
+        window.location.href = "parabens.html"
+        break;
+    }else { 
+            alert("Digite uma opção válida, 1 ou 2!")
+    }
+    }
+    }
+    
+    
+     
+    
+    
+    
+    
