90d3186a4fe04a29269e4aa928dab471caf7f28d
Elica <elicavb@gmail.com>
2022-04-22 12:16:31 -0300

    Meu primeiro jogo

--- drestranho1.js
+++ drestranho1.js
@@ -1,15 +1,15 @@
 
-function respostaCerta(){
-while(opcao !=1 || opcao!=2 ){
-var opcao = prompt("Digite a resposta certa!")
-    if(opcao == 2){
-        location.href = "drestranho2.html"
-    break;
-}else if (opcao == 1){
-    location.href = "gameover.html"
-    break;
-}else { 
-        alert("Digite uma opção válida, 1 ou 2!")
-}
-}
+function respostaCerta() {
+    while (opcao != 1 || opcao != 2) {
+        var opcao = prompt("Digite a resposta certa!")
+        if (opcao == 2) {
+            location.href = "drestranho2.html"
+            break;
+        } else if (opcao == 1) {
+            location.href = "gameover.html"
+            break;
+        } else {
+            alert("Digite uma opção válida, 1 ou 2!")
+        }
+    }
 }
